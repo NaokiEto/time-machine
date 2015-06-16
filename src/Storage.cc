ca72032572c4da33026cf447bd5b66568bb0db27
Lakers <neto@caltech.edu>
2015-06-15 23:13:24 -0400

    Added the vlan patch

--- src/Storage.cc
+++ src/Storage.cc
@@ -390,6 +390,8 @@ void Storage::addPkt(const struct pcap_pkthdr *header,
         
 
 		idxpacket += 4; // Skip the vlan header
+        packet += 4;
+
 		//pkt_hdr_size = 0;
 
 		// Check for 802.1ah (Q-in-Q) containing IP.
@@ -398,7 +400,10 @@ void Storage::addPkt(const struct pcap_pkthdr *header,
 		// specification that allows for deeper
 		// nesting.
 		if ( ((idxpacket[2] << 8) + idxpacket[3]) == 0x0800 )
+        {
 			idxpacket += 4;
+            packet += 4;
+        }
     }
 
     // TODO: Cannot handle MPLS labels just yet
@@ -412,6 +417,7 @@ void Storage::addPkt(const struct pcap_pkthdr *header,
 			{
 			end_of_stack = *(idxpacket + 2) & 0x01;
 			idxpacket += 4;
+            packet += 4;
 			}
 		}
     
